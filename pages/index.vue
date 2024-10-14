fd2c03629aeb3ce6fbf4ab7cfa84cfb8ea2abe80
tim-bistroo <timhaerkens@gmail.com>
2024-10-14 20:41:56 +0200

    filters added to offer store

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,10 @@ import OfferCard from '~/components/OfferOverview/OfferCard.vue'
 
 const offersStore = useOffersStore()
 
+const possibleLocations = computed(() => offersStore.possibleLocations)
+const possibleLevels = computed(() => offersStore.possibleLevels)
+const possibleDepartments = computed(() => offersStore.possibleDepartments)
+
 watch(
   () => offersStore.filters,
   () => {
@@ -26,6 +30,38 @@ watch(
       placeholder="Zoek op functietitel" 
       icon="i-heroicons-magnifying-glass-20-solid"
     />
+
+    <div class="flex flex-col md:flex-row md:justify-between gap-4">
+      <USelectMenu 
+        v-model="offersStore.filters.levels" 
+        :options="possibleLevels" 
+        multiple 
+        class="w-full"
+        placeholder="Kies niveau(s)" />
+
+      <USelectMenu 
+        v-model="offersStore.filters.locations" 
+        :options="possibleLocations" 
+        class="w-full"
+        multiple 
+        placeholder="Kies locatie(s)" />
+
+      <USelectMenu 
+        v-model="offersStore.filters.departments" 
+        :options="possibleDepartments" 
+        class="w-full"
+        multiple 
+        placeholder="Kies afdeling(en)" />
+
+      <UInput 
+        v-model="offersStore.filters.minSalary" 
+        size="sm" 
+        type="number"
+        class="w-full"
+        icon="i-heroicons-currency-euro-20-solid"
+        placeholder="Min. salaris" />
+        
+    </div>
     
   </header>
     
--- stores/offers.ts
+++ stores/offers.ts
@@ -1,5 +1,7 @@
 import { defineStore } from 'pinia'
 import type { Offer } from '~/types/offer'
+const { experienceLevels } = useExperienceLevels()
+
 
 export const useOffersStore = defineStore('offers', {
   state: () => ({
@@ -9,6 +11,10 @@ export const useOffersStore = defineStore('offers', {
     error: null as Error | null,
     filters: {
         searchQuery: '' as string,
+        locations: [] as string[],
+        levels: [] as string[],
+        minSalary: 0 as number,
+        departments: [] as string[],
       },
   }),
   actions: {
@@ -35,6 +41,33 @@ export const useOffersStore = defineStore('offers', {
             offer.title.toLowerCase().includes(query)
           )
         }
+
+        // Filter by location
+        if (this.filters.locations.length > 0) {
+            offers = offers.filter((offer) =>
+            this.filters.locations.includes(offer.city)
+            )
+        }
+
+        // Filter by level
+        if (this.filters.levels.length > 0) {
+            offers = offers.filter((offer) =>
+            this.filters.levels.includes(experienceLevels[offer.experience_code])
+            )
+        }
+
+        // Filter by salary
+        if (this.filters.minSalary > 0) {
+            offers = offers.filter((offer) => offer.salary.min >= this.filters.minSalary)
+        }
+
+        // Filter by department
+        if (this.filters.departments.length > 0) {
+            offers = offers.filter((offer) =>
+            this.filters.departments.includes(offer.department)
+            )
+        }
+
         this.filteredOffers = offers
       },
   },
@@ -42,5 +75,26 @@ export const useOffersStore = defineStore('offers', {
     getOfferById: (state) => (id: number) => {
       return state.offers.find((offer) => offer.id === id)
     },
+    possibleLocations: (state) => {
+        const cities = state.offers
+          .map((offer) => offer.city)
+          .filter((city) => city && city.trim() !== '')
+  
+        return Array.from(new Set(cities)).sort()
+    },
+    possibleLevels: (state) => {
+        const levels = state.offers
+          .map((offer) => offer.experience_code)
+          .filter((level) => level && level.trim() !== '').map((level) => experienceLevels[level])
+  
+        return Array.from(new Set(levels)).sort()
+    },
+    possibleDepartments: (state) => {
+        const departments = state.offers
+          .map((offer) => offer.department)
+          .filter((department) => department && department.trim() !== '')
+  
+        return Array.from(new Set(departments)).sort()
+    }
   },
 })
